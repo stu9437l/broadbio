3ebe61a296a02c3c09bba976557d89e1017fda32
Lal Bahadur Budhachhetri <lalbahadurbc@neputer.com>
2023-05-16 19:02:14 +0545

    homepage slicing

---
+++ script/script.js
@@ -0,0 +1,19 @@
+// offcanvas menu dropdwown toggle
+
+const menuLink = $(".offcanvas--menu .nav-list .nav__link");
+let toggleIcon = true;
+
+menuLink.each(function () {
+  $(this).click(function () {
+    $(this).closest(".nav__item").find(".dropdown-list").slideToggle();
+    menuLink.find("i").removeClass("fa-chevron-down");
+    if (toggleIcon) {
+      toggleIcon = false;
+      $(this).closest(".nav__item").find("i").addClass("fa-chevron-up");
+    } else {
+      toggleIcon = true;
+      $(this).closest(".nav__item").find("i").addClass("fa-chevron-down");
+    }
+  });
+});
+console.log(toggleIcon);
